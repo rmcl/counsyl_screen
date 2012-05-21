d95b198a29d92fb77eb3f5c078f5321f90de632c
Russell McLoughlin <russ.mcl@gmail.com>
2012-05-20 22:35:26 -0700

    amino_test.py

--- amino_test.py
+++ amino_test.py
@@ -8,12 +8,22 @@ Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 """
 
 import unittest
-
+import amino
 
 class AminoTest(unittest.TestCase):
-	def test_codon2name(self):
-		
-
+    def test_is_single_sub(self):
+        tests = """
+        russell rassell
+        bob bab
+        UGA UAG
+        AUG AGG
+        """.strip().splitlines()
+        tests = map(lambda x: x.split(), tests)
+        
+        truth = [True, True, False, True]
+        
+        for t in xrange(len(tests)):
+            self.assertEquals(amino.is_single_sub(tests[t][0],tests[t][1]),bool(truth[t]))
     
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
